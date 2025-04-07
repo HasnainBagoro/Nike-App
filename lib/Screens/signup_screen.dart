c5261fff95d49ebf759464763902b90d9fad10d9
HasnainBagoro <mhassuu143@gmail.com>
2025-04-07 17:27:33 +0500

    fj

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -28,6 +28,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
               width: 150,
               height: 150,
             ),
+            
             const Text(
               'Signup',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
