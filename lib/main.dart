b1c1a381fd3d7c6e1c3df86bbf79bad65aa4ff4c
HasnainBagoro <mhassuu143@gmail.com>
2025-04-12 16:52:37 +0500

    Integrate Firebase into the project; update dependencies, initialize Firebase in main, and configure Google services

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nike/Screens/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp;
   runApp(const MyApp());
 }
 
